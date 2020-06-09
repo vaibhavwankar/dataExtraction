af98857fff80c5b982cded868166ec20a823a7af
vaibhavwankar <wankar333@gmail.com>
2020-06-09 17:04:32 +0530

    removed cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals, print_function
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
@@ -22,8 +21,6 @@ import win32com.client
 
 app = Flask(__name__)
 api = Api(app)
-
-CORS(app)
 
 @app.route("/health")
 def hello():
